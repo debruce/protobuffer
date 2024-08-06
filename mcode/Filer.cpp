75b3e4f481410e863a0ac8a36c3076a766b033de
David Bruce <david.e.bruce@icloud.com>
2024-08-06 15:48:48 -0700

    blah

--- mcode/Filer.cpp
+++ mcode/Filer.cpp
@@ -17,10 +17,11 @@
 #include <iostream>
 
 using namespace std;
+using namespace std::filesystem;
 
-struct MyExcept : public std::system_error {
-    MyExcept(const string& scall_name)
-        : system_error(error_code(errno, generic_category()), scall_name) {}
+struct MyExcept : public filesystem_error {
+    MyExcept(const string& what, const path& path)
+        : filesystem_error(what, path, error_code(errno, generic_category())) {}
 };
 
 using DataVector = Filer::DataVector;
@@ -41,23 +42,23 @@ inline size_t& as_size_t(void* ptr)
 }
 
 struct Filer::Impl {
-    filesystem::path temp_dir_;
-    filesystem::path data_dir_;
+    path temp_dir_;
+    path data_dir_;
     int temp_fd_;
     int data_fd_;
     int note_fd_;
     int watch_token_;
     thread* tid;
 
-    Impl(const filesystem::path& root_dir)
+    Impl(const path& root_dir)
     {
         data_dir_ = root_dir;
         temp_dir_ = root_dir / "temp";
-        filesystem::create_directories(temp_dir_);
+        create_directories(temp_dir_);
         if ((temp_fd_ = open(temp_dir_.c_str(), O_DIRECTORY|O_RDONLY)) < 0)
-            throw MyExcept("open");
+            throw MyExcept("open", temp_dir_);
         if ((data_fd_ = open(data_dir_.c_str(), O_DIRECTORY|O_RDONLY)) < 0)
-            throw MyExcept("open");
+            throw MyExcept("open", data_dir_);
         note_fd_ = inotify_init();
     }
 
@@ -84,12 +85,12 @@ struct Filer::Impl {
         snprintf(file_name, sizeof(file_name), "%0lx", t);
         int fd = openat(temp_fd_, file_name, O_CREAT|O_RDWR, 0644);
         if (fd < 0) {
-            throw MyExcept("openat");
+            throw MyExcept("openat", temp_dir_ / file_name);
         }
         ftruncate(fd, byte_count);
         auto orig_ptr = mmap(0, byte_count, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
         if (orig_ptr == MAP_FAILED) {
-            throw MyExcept("mmap");
+            throw MyExcept("mmap", temp_dir_ / file_name);
         }
         auto ptr = orig_ptr;
         as_size_t(ptr) = data.size();
@@ -103,13 +104,13 @@ struct Filer::Impl {
         }
 
         if (munmap(orig_ptr, byte_count) < 0) {
-            throw MyExcept("mumap");
+            throw MyExcept("mumap", temp_dir_ / file_name);
         }
         if (linkat(temp_fd_, file_name, data_fd_, name.c_str(), 0) < 0) {
-            throw MyExcept("linkat");
+            throw MyExcept("linkat", data_dir_ / name);
         }
         if (unlinkat(temp_fd_, file_name, 0) < 0) {
-            throw MyExcept("unlinkat");
+            throw MyExcept("unlinkat", temp_dir_ / file_name);
         }
         close(fd);
     }
@@ -139,7 +140,7 @@ struct Filer::Impl {
             data[i] = string(p, bcount);
         }
         if (munmap(orig_ptr, ss.st_size) < 0) {
-            throw MyExcept("mumap");
+            throw MyExcept("mumap", data_dir_ / name);
         }
         return true;
     }
@@ -153,13 +154,13 @@ struct Filer::Impl {
     {
         if (state) {
             if ((watch_token_ = inotify_add_watch(note_fd_, data_dir_.c_str(), IN_OPEN|IN_CREATE)) < 0) {
-                throw MyExcept("inotify_add_watch");
+                throw MyExcept("inotify_add_watch", data_dir_);
             }
             tid = new thread([&]() { this->wait(); });
         }
         else {
             if (inotify_rm_watch(note_fd_, watch_token_) < 0) {
-                throw MyExcept("inotify_rm_watch");
+                throw MyExcept("inotify_rm_watch", data_dir_);
             }
         }
     }
--- mcode/main.cpp
+++ mcode/main.cpp
@@ -7,20 +7,25 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    auto files = filesystem::path{"/run/user/1000"} / "filer";
-    cout << "temp = " << files << endl;
-    Filer f(files);
+    try {
+        auto files = filesystem::path{"/run/user/1000"} / "filer";
+        Filer f(files);
+        cout << "after constructor" << endl;
 
-    f.watch();
+        f.watch();
 
-    for (auto i = 0; i < 5; i++) {
-        sleep(1);
-        f.send("x", vector<string>{"x", "y"});
-        sleep(1);
+        for (auto i = 0; i < 5; i++) {
+            sleep(1);
+            f.send("x", vector<string>{"x", "y"});
+            sleep(1);
 
-        vector<string> data;
-        auto v = f.read("x", data);
-        // cout << "v=" << v << " first=" << data[0] << " second=" << data[1] << endl;
-        f.remove("x");
+            vector<string> data;
+            auto v = f.read("x", data);
+            cout << "v=" << v << " first=" << data[0] << " second=" << data[1] << endl;
+            f.remove("x");
+        }
+    }
+    catch (const std::exception& ex) {
+        cout << "Threw " << ex.what() << endl;
     }
 }
